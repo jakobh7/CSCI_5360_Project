a76ef151977d3d1cad62dc9c40cfed8f1f3205f0
Jake Bernstein <jkbrnstn@gmail.com>
2019-11-27 17:07:47 -0600

    Create hashTest.py
    This is the ecnryption algo I want to use. Having trouble with Crypto lib.

---
+++ hashTest.py
@@ -0,0 +1,28 @@
+# import hashlib
+# text = "0.0.0.0"
+# print(text)
+# m = hashlib.md5(text.encode('utf-8'))
+# print(m.hexdigest())
+
+from Crypto.Cipher import AES
+from base64 import *
+
+MASTER_KEY="MDS is cool!"
+
+def encrypt_val(clear_text):
+    enc_secret = AES.new(MASTER_KEY[:32])
+    tag_string = (str(clear_text) +
+                  (AES.block_size -
+                   len(str(clear_text)) % AES.block_size) * "\0")
+    cipher_text = base64.b64encode(enc_secret.encrypt(tag_string))
+
+    return cipher_text
+
+def decrypt_val(cipher_text):
+    dec_secret = AES.new(MASTER_KEY[:32])
+    raw_decrypted = dec_secret.decrypt(base64.b64decode(cipher_text))
+    clear_val = raw_decrypted.decode().rstrip("\0")
+    return clear_val
+
+if __name__ == "__main__":
+    print(decrypt_val(encrypt_val("Test")))
