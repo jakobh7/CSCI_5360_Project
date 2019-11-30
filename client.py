283dd6a54b34e45607d1a476f3a071772541ae77
Jakob Horner <jakobh7@aol.com>
2019-11-30 15:48:07 -0600

    changed server and client to use python http.server/client libraries

--- client.py
+++ client.py
@@ -1,7 +1,14 @@
 #code from PubNub blog https://www.pubnub.com/blog/socket-programming-in-python-client-server-p2p/
 from socket import *
 from io import BytesIO
+from http import client
 
+conn = client.HTTPConnection("localhost", 8080)
+conn.request("GET", "/")
+response = conn.getresponse()
+print(response)
+
+'''
 text = "I am CLIENT\n"
 bytes_text = bytes(text, 'utf-8')
 
@@ -17,3 +24,4 @@ client.close()
 from_server = bytes_from_server.decode('utf-8')
 
 print(from_server)
+'''
--- server.py
+++ server.py
@@ -1,7 +1,27 @@
 #code from PubNub blog https://www.pubnub.com/blog/socket-programming-in-python-client-server-p2p/
 from socket import *
 from io import BytesIO
+from http import server
 
+class RequestHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_content("Hello")
+
+    def send_content(self, content):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Content-length", str(len(content)))
+        self.end_headers()
+        send.wfile.write(content)
+
+
+if __name__ == '__main__':
+   serverAddress = ('', 8080)
+   server = server.HTTPServer(serverAddress, RequestHandler)
+   server.serve_forever()
+   print("Serving on Port 8080...")
+
+'''
 text = "I am SERVER\n"
 bytes_text = bytes(text, 'utf-8')
 
@@ -25,3 +45,4 @@ while True:
 
     conn.close()
     print('client disconnected')
+    '''
